b14c9e5bdf2d2a2632e1cf4d20cbd2dd2e64abc9
DionatanLeao <dionatan.ti@outlook.com>
2020-11-22 21:23:01 -0300

    Serialização Java

---
+++ src/br/com/alura/java/io/test/IMC.java
@@ -0,0 +1,43 @@
+package br.com.alura.java.io.test;
+
+import java.util.Scanner;
+
+public class IMC {
+
+	private static float peso;
+	private static float altura;
+	private static float imc;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite seu Peso: ");
+		peso = sc.nextFloat();
+		System.out.print("Digite sua Altura: ");
+		altura = sc.nextFloat();
+
+		imc = peso / (altura * altura);
+		System.out.println(imc);
+
+		if (imc < 18.5)
+			System.out.println("Abaixo do Peso");
+
+		else if (imc >= 18.5 && imc <= 24.9)
+			System.out.println("Peso Normal");
+
+		else if (imc == 25 && imc <= 29.9)
+			System.out.println("Sobrepeso");
+
+		else if (imc == 30 && imc <= 34.9)
+			System.out.println("Obesidade Grau I");
+
+		else if (imc == 35 && imc <= 39.9)
+			System.out.println("Obesidade Grau II");
+
+		else if (imc > 40)
+			System.out.println("Obesidade Grau III ou Mórbida");
+
+	}
+
+}
---
+++ src/br/com/alura/java/io/test/SaldoInsufucienteException.java
@@ -0,0 +1,11 @@
+package br.com.alura.java.io.test;
+
+public class SaldoInsufucienteException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public SaldoInsufucienteException(String msg) {
+		super(msg);
+	}
+
+}
